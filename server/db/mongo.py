b03537004c792bce59a39b0603bd1039f251b311
StephenTang <stephen.tang@clarity.ca>
2025-11-22 17:30:35 -0500

    install pymongo

---
+++ server/db/mongo.py
@@ -0,0 +1,10 @@
+import os
+from pymongo import MongoClient
+
+mongo_uri = os.environ["MONGODB_URI"]
+db_name = os.environ["MONGODB_DB_NAME"]
+
+client = MongoClient(mongo_uri)
+db = client[db_name]
+
+users_collection = db["users"]
