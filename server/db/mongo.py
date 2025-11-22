5bd5be9d22abe992880beecdf8aa6e92bfa09f1d
StephenTang <stephen.tang@clarity.ca>
2025-11-22 17:47:38 -0500

    basic test for db

--- server/db/mongo.py
+++ server/db/mongo.py
@@ -2,7 +2,7 @@ import os
 from pymongo import MongoClient
 
 mongo_uri = os.environ["MONGODB_URI"]
-db_name = os.environ["MONGODB_DEV_DB_NAME"]
+db_name = os.environ["MONGODB_SANDBOX_DB_NAME"]
 
 client = MongoClient(mongo_uri)
 db = client[db_name]
